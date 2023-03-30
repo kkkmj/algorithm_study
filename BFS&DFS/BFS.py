a5234fcc9a60b085f65107a12ec6e5993bf40dbb
kkkmj <wjsdus531@naver.com>
2023-03-31 04:43:50 +0900

    0330 BFS&DFS

---
+++ BFS&DFS/BFS.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def bfs(graph, node, visited):
+    queue = deque([node])
+    visited[node] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not(visited[i]):
+                queue.append(i)
+                visited[i] = True
+
+visited = [False] * 9
+
+graph = [
+    [],
+    [2, 3],
+    [1, 8],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7, 8],
+    [6, 8],
+    [2, 6, 7]
+]
+
+bfs(graph, 1, visited)
