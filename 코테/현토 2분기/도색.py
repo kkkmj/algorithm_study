087246f3c6206886d95631524e124b01021201ab
kkkmj <wjsdus531@naver.com>
2023-05-27 03:57:08 +0900

    0526 문자열 1, BFS 1

--- 코테/현토 2분기/도색.py
+++ 코테/현토 2분기/도색.py
@@ -3,6 +3,8 @@ n, m = map(int, input().split())
 cars = [0]+list(map(int, input().split()))
 total_car = [0]*(n+1)
 #아마 100000
+
+# 뭐 누적합을 왜 사용했는지에 관련해서 물어볼 수 도 있을 듯
 for i in range(1,n+1):
     total_car[i]=total_car[i-1]+cars[i]
 
--- 코테/현토 2분기/컨테이너.py
+++ 코테/현토 2분기/컨테이너.py
@@ -6,7 +6,7 @@
 n = int(input())
 words = [input().strip() for _ in range(n)]
 words.sort(key=lambda x:len(x)) #...이거 sort해주면 안됐네ㅋㅋㅋㅋㅋㅋ 왜냐면 사전순 그래도 해주는거니까..아..ㅋㅋ틀렷다
-
+# 사실 누적합 왜썼냐는 질문 보다는 2번 왜이렇게 풀었는지가 너무너무 궁금할 듯 나같아도 궁금하다..sibal
 min_len = len(words[0])
 
 for i in range(n):
@@ -17,7 +17,8 @@ for i in range(n):
     else:
         for j in range(i):
             if words[j] in word[1:-1]:
-                word = word.replace(words[j],"")
+                word = word.replace(words[j],"") #우선 이거 replace 해주면 안됨
+                #그럼 어떻게 구현을 할 것이냐... ㅠ
                 for k in range(i):
                     if word==words[k]:
                         flag=1
